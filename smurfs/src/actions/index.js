56739ee289d5c282bb5bd40989a9118d23d1acc2
Sally Sal <sallysal@alexs-imac.attlocal.net>
2019-09-13 10:43:18 -0700

    created axios call inside actions folder

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const FETCHING_SMURFS_START = "FETCHING_SMURFS_START";
+export const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: Fetching_SMUFDS_START });
+    axios
+        .get("http://localhost:3333/smurfs/")
+        .then(res => {
+            dispatch({ 
+                type: FETCHING_SMURFS_SUCCESS,
+                PAYLOAD: res.data });
+        })
+}
