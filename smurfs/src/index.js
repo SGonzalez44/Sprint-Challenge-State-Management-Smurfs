7a3e5339408b1d95257cd9473be6214fe29cea06
Sally Sal <sallysal@alexs-imac.attlocal.net>
2019-09-13 09:49:30 -0700

    set up initial store and reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore } from 'redux';
+
+function reducer() {
+    return {
+        title: "Reduc in Sals Mind"
+    };
+}
+
+
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById("root"));
