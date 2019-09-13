022278b6a77baa7d4325fe7e840f9d662720a495
Sally Sal <sallysal@alexs-imac.attlocal.net>
2019-09-13 11:06:39 -0700

    adding middleware NOT finished

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,18 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import {Provider} from 'react-redux';
 
-function reducer() {
-    return {
-        title: "Reduc in Sals Mind"
-    };
-}
+const composeEnhancer = window.__REDUX__DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    composeEnhancer(applyMiddleware(thunk))
+    );
 
 ReactDOM.render(
 <Provider store={store}>
