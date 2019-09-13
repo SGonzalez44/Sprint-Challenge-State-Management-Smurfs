df65c87a55e6b5e572d667e3e33abcffbab642c3
Sally Sal <sallysal@alexs-imac.attlocal.net>
2019-09-13 10:31:45 -0700

    updated smurfs component

--- smurfs/src/components/smurfs.js
+++ smurfs/src/components/smurfs.js
@@ -2,8 +2,16 @@ import React from "react";
 import {connect} from "react-redux";
 
 const smurfs = props =>{
-    <>
-    </>
+    return
+    <div>
+        <h2></h2>
+    </div>
 }
 
-export default connect(null{})(smurfs);
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, {})(smurfs);
